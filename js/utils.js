fd3dfcf66d810ee255913505f6648a34fbf1d9de
virtualarchitectures <virtualarchitectures@users.noreply.github.com>
2025-06-13 11:28:42 +0100

    Organised and documented functions in utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,35 +1,67 @@
-// Geolocation
+//----------Geolocation Functions----------//
+
+// Function to get the current location of the user
 function getGeolocation() {
   if (navigator.geolocation) {
+    // If geolocation is available, set up position tracking
     navigator.geolocation.getCurrentPosition(showPosition, failPosition);
     navigator.geolocation.watchPosition(trackPosition);
     console.log("Geo location enabled");
   } else {
+    // Handle case when geolocation is not supported
     console.log("Geo location failed");
     geoEnabled.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
+// Function to handle successful geolocation
 function showPosition(position) {
+  // Show position when geolocation is enabled
   geoEnabled.innerHTML = "geolocation enabled";
   geoEnabled.style.visibility = "hidden";
   var s = document.getElementById("shield");
+  // Hide shield if geolocation is enabled
   s.style.visibility = "hidden";
 }
 
+// Function to handle geolocation failures
 function failPosition(error) {
+  // Handle geolocation failures
   if (error.code == error.PERMISSION_DENIED) {
     console.log("Geolocation access denied");
-    // you could hide elements, but currently covered by the shield
   } else {
     console.log("Other error");
   }
 }
 
+// Function to handle geolocation updates
 function trackPosition(position) {
+  // Update global variable with current position
   window.currPosition = position;
 }
 
+//----------Map Visualisation Functions----------//
+
+// Function to update the map with new GeoJSON data
+function mapJson() {
+  // Clear existing data in the geoJsonLayer
+  geoJsonLayer.clearLayers();
+
+  // Add new data to the geoJsonLayer
+  geoJsonLayer.addData(myJson);
+
+  // Adjust map view to fit the newly added data
+  if (myJson.features.length > 0) {
+    var bounds = geoJsonLayer.getBounds();
+    map.fitBounds(bounds);
+  }
+
+  console.log("Map updated with new data.");
+}
+
+//----------Data Preparation Functions----------//
+
+// Function to generate a timestamp for saving data
 function getSaveDate() {
   let saveDate = new Date();
   let yr = currDate.getFullYear();
@@ -39,6 +71,7 @@ function getSaveDate() {
   let mn = currDate.getMinutes();
   let sc = currDate.getSeconds();
 
+  // Format date/time values with leading zeros if necessary
   if (mo < 10) {
     mo = "0" + mo;
   }
@@ -58,46 +91,40 @@ function getSaveDate() {
   return yr + "-" + mo + "-" + dt + "-" + hr + "-" + mn + "-" + sc;
 }
 
-function mapJson() {
-  // Clear existing data in the geoJsonLayer
-  geoJsonLayer.clearLayers();
+//----------Data Export Functions----------//
 
-  // Add new data
-  geoJsonLayer.addData(myJson);
-
-  // Adjust map view to fit the new data
-  if (myJson.features.length > 0) {
-    var bounds = geoJsonLayer.getBounds();
-    map.fitBounds(bounds);
-  }
-
-  console.log("Map updated with new data.");
-}
-
-// CSV Export
+// CSV Export Functions
 function exportCSV() {
   let csvContent = "data:text/csv;charset=utf-8,";
 
+  // Convert each data array to a CSV row and append to CSV content
   dataArr.forEach(function (rowArr) {
     let row = rowArr.join(",");
     csvContent += row + "\r\n";
   });
 
+  // Encode CSV content as a URI and open it in a new window
   var encodedUri = encodeURI(csvContent);
   window.open(encodedUri);
 }
 
 function exportCSV2() {
+  // Create an initialized CSV content string
   let csvContent;
 
+  // Convert each data array to a CSV row and append to CSV content
   dataArr.forEach(function (rowArr) {
     let row = rowArr.join(",");
     csvContent += row + "\r\n";
   });
 
+  // Get a formatted current date string
   let dateStr = getSaveDate();
+
+  // Create a Blob for the CSV data
   const blobData = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
 
+  // Set up FileReader to read the Blob and trigger a download
   var reader = new FileReader();
   reader.onload = function () {
     var popup = window.open();
@@ -110,29 +137,29 @@ function exportCSV2() {
   reader.readAsDataURL(blobData);
 }
 
-// GeoJSON Export
+// GeoJSON Export Functions
 function exportJson() {
   console.log("export geojson...");
   console.log(myJson);
 
-  // Convert object to Blob
+  // Convert GeoJSON object to a Blob
   const blobData = new Blob([JSON.stringify(myJson, undefined, 2)], {
     type: "text/json;charset=utf-8",
   });
 
-  // Convert Blob to URL
+  // Create a URL for the Blob
   const blobUrl = URL.createObjectURL(blobData);
 
-  // Create an a element with blobl URL
+  // Create a link element to trigger a download
   const anchor = document.createElement("a");
   anchor.href = blobUrl;
   anchor.target = "_self";
   anchor.download = "datawalking.geojson";
 
-  // Auto click on a element, trigger the file download
+  // Trigger the file download
   anchor.click();
 
-  // Don't forget ;)
+  // Clean up the Blob URL
   URL.revokeObjectURL(blobUrl);
 }
 
@@ -140,14 +167,15 @@ function exportJson2() {
   console.log("export geojson new way 2...");
   console.log(myJson);
 
-  let dateStr = getSaveDate(); //yr+"-"+mo+"-"+dt+"-"+hr+"-"+mn+"-"+sc;
+  // Get a formatted current date string
+  let dateStr = getSaveDate();
 
-  // Convert object to Blob
+  // Convert GeoJSON object to a Blob
   const blobData = new Blob([JSON.stringify(myJson, undefined, 2)], {
     type: "text/json;charset=utf-8",
   });
 
-  //
+  // Set up FileReader to read the Blob and trigger a download
   var reader = new FileReader();
   reader.onload = function () {
     var popup = window.open();
